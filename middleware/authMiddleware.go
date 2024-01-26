7b44b26a2e84c1fb29b51735cf7860d2eb1bd795
sss360 <sss360@gmail.com/>
2024-01-26 13:09:23 +0530

    middleware fixed

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -18,5 +18,17 @@ func Authenticate() gin.HandlerFunc {
 			return
 		}
 		helper.ValidateToken(clientToken)
+		claim, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Token not found"})
+			c.Abort()
+			return
+		}
+		c.Set("email", claim.Email)
+		c.Set("first_name", claim.First_name)
+		c.Set("last_name", claim.Last_name)
+		c.Set("uid", claim.Uid)
+		c.Set("user_type", claim.User_type)
+		c.Next()
 	}
 }
